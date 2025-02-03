af14d3631c6cdb2479e5ea23e1b52fbc35522c0a
Denis Kramarenko <cashflow.dkm@gmail.com>
2025-02-03 17:31:47 +1000

    Finish work

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
-"use strict"
+'use strict';
+
+const btnsReset = document.getElementsByClassName('btn__modal');
+const btnCancel = document.getElementById('-btn__cancel')
+
+const modal = document.getElementById('-modal__wrap');
+// Code
+
+for (let i = 0; i < btnsReset.length; i++) {
+  btnsReset[i].addEventListener('click', () => {
+    modal.classList.remove('modal__wrap_hidden');
+  });
+}
+
+btnCancel.addEventListener('click', () => {
+  modal.classList.add('modal__wrap_hidden');
+})
